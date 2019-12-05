4984738704a62dbba6ec5a637274c56d5dc21e9b
nemanjan00 <nemanjan00@gmail.com>
2019-12-05 22:29:10 +0100

    ADB

---
+++ dev/download.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PKGBUILD=`curl https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=android-sdk-platform-tools | fgrep pkgver | head -n1 | cut -d"=" -f2`
+
+wget https://dl.google.com/android/repository/platform-tools_r$PKGBUILD-linux.zip -O /tmp/adb.zip
+
+mkdir -p binary
+
+unzip /tmp/adb.zip -d binary
+
